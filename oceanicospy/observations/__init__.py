065449eb514d2800732719ecb0cb3aa13808749a
Daniela Rosero <lroserom@unal.edu.co>
2025-08-08 12:36:15 -0500

    test

--- oceanicospy/observations/__init__.py
+++ oceanicospy/observations/__init__.py
@@ -3,3 +3,5 @@ from .rbr import RBR
 from .awac import AWAC
 from .weather_station import WeatherStation
 from .resistive_waterlevel_sensor import ResistiveSensor
+from .buoy import Buoy
+from .mantaray import Mantaray
--- oceanicospy/retrievals/__init__.py
+++ oceanicospy/retrievals/__init__.py
@@ -1,2 +1,3 @@
 from .download_ERA5_data import *
 from .download_uhslc_data import *
+from .download_CMDS_data import *
