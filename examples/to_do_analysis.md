627d07e15a6a3d55c312c61617c9f95cf5d1f236
oceanicos <oceanicos_med@unal.edu.co>
2026-03-17 15:42:25 -0500

    adding EMD to temporal

--- examples/to_do_analysis.md
+++ examples/to_do_analysis.md
@@ -6,3 +6,10 @@
 - Verifying the computation of Hs for ig and sw band
 - Finishing the plotting for scalograms
 - Explain a bit better/more the .ipynb
+
+# Things to do in temporal analysis
+
+- Do we really need a detrended time series?
+- Verify tt variable in upcrossing method (sampling frequency might not be required)
+- check properly the use of the EMD using the example I have.
+- finishing the .ipynb for this section
--- oceanicospy/analysis/temporal.py
+++ oceanicospy/analysis/temporal.py
@@ -28,52 +28,17 @@ class WaveTemporalAnalyzer:
 
         23-Feb-2014 : First Matlab version - Daniel Peláez
         01-Sep-2023 : First Python version - Alejandro Henao
-        10-Dec-2024 : Polishing            - Franklin Ayala 
+        10-Dec-2025 : Empirical Mode decomposition - Franklin Ayala 
 
         """
         self.measured_signal = measured_signal
         self.sampling_data = sampling_data
         self.surface_level_column = surface_level_column
 
-    def compute_params_from_zero_upcrossing(self):
-        """
-        Calculate wave parameters from zero-crossing analysis of pressure data.
-
-
-        Returns
-        -------
-        pandas.DataFrame
-            DataFrame with wave parameters indexed by time, containing columns:
-            - 'H1/3': Significant wave height (H1/3).
-            - 'Tmean': Mean wave period (Tmean).
-        """
-        wave_params=["time","H1/3","Tmean"]
-        wave_params_data={param:[] for param in wave_params}
-
-        for i in self.measured_signal['burstId'].unique()[:2]:
-            burst_signal = self.measured_signal[self.measured_signal['burstId'] == i]
-
-            # TODO: validate this step, detrend option is already given in the observations. What if the data is coming from another source?
-            burst_signal_detrended = burst_signal.iloc[:,:-1].apply(lambda x: detrend(x,type='constant'), axis=0)
-            burst_signal_detrended[self.measured_signal.columns[-1]] = burst_signal.iloc[:, -1]
-            print(burst_signal_detrended)
-
-            H_top_third, Hmax, Tmean, Lmean = self.apply_zero_upcrossing_burst(burst_signal_detrended['pressure[bar]'], self.sampling_data['sampling_freq'],
-                                    self.sampling_data['anchoring_depth'], self.sampling_data['sensor_height'])
-
-            wave_params_data['time'].append(burst_signal_detrended.index[0])
-            wave_params_data['H1/3'].append(H_top_third)
-            wave_params_data['Tmean'].append(Tmean)
-            print(wave_params_data['time'])
-
-        wave_params_data=pd.DataFrame(wave_params_data).set_index('time')
-
-        return wave_params_data
-
     def apply_zero_upcrossing_burst(self, burst_signal, sampling_freq, anchoring_depth, sensor_height):
         """
         This function calculates the significant wave height, the period and the wavelength
-        with the zero-crossing method.
+        with the zero-upcrossing method.
         
         Parameters
         ----------
@@ -141,24 +106,86 @@ class WaveTemporalAnalyzer:
 
         return H_top_third,Hmax,Tmean,Lmean
 
-#     def decompose_into_IMFs_for_bursts(self,emd_type,maximum_IMFs,number_ensembles=None,amplitude_noise_std=None):
-#         hourly_timeindex = self.measurement_signal.index.floor('h').unique().sort_values()
-#         time_seconds = np.arange(0,self.burst_length_s,1)
+    def compute_params_from_zero_upcrossing(self):
+        """
+        Calculate wave parameters from zero-crossing analysis of pressure data.
 
-#         IMFs_all = np.zeros((len(hourly_timeindex),maximum_IMFs,self.burst_length_s))
 
-#         if 'burstId' in self.measurement_signal.columns:
-#             for idx,burst in enumerate(self.measurement_signal["burstId"].unique()):
-#                 burst_series = self.measurement_signal[self.measurement_signal['burstId'] == burst]
-#                 if emd_type == 'EMD':
-#                     emd = EMD()
-#                     IMFs = emd(burst_series['eta[m]'].values, time_seconds, max_imf=maximum_IMFs)[:maximum_IMFs, :]
-#                 elif emd_type == 'EEMD':
-#                     eemd = EEMD(trials=number_ensembles, epsilon=amplitude_noise_std)
-#                     IMFs = eemd(burst_series['eta[m]'].values, time_seconds, max_imf=maximum_IMFs)[:maximum_IMFs, :]
-#                 else:
-#                     ceemd = CEEMDAN(DTYPE=np.float16,trials=number_ensembles,epsilon=amplitude_noise_std,parallel=True,processes=48)
-#                     IMFs = ceemd(burst_series['eta[m]'].values,time_seconds,max_imf=maximum_IMFs)[:maximum_IMFs,:]
-#                 IMFs_all[idx,:,:] = IMFs
-#         return IMFs_all
+        Returns
+        -------
+        pandas.DataFrame
+            DataFrame with wave parameters indexed by time, containing columns:
+            - 'H1/3': Significant wave height (H1/3).
+            - 'Tmean': Mean wave period (Tmean).
+        """
+        wave_params=["time","H1/3","Tmean"]
+        wave_params_data={param:[] for param in wave_params}
+
+        for i in self.measured_signal['burstId'].unique():
+            burst_signal = self.measured_signal[self.measured_signal['burstId'] == i]
+
+            # TODO: validate this step, detrend option is already given in the observations. What if the data is coming from another source?
+            burst_signal_detrended = burst_signal.iloc[:,:-1].apply(lambda x: detrend(x,type='constant'), axis=0)
+            burst_signal_detrended[self.measured_signal.columns[-1]] = burst_signal.iloc[:, -1]
+
+            H_top_third, Hmax, Tmean, Lmean = self.apply_zero_upcrossing_burst(burst_signal_detrended['pressure[bar]'], self.sampling_data['sampling_freq'],
+                                    self.sampling_data['anchoring_depth'], self.sampling_data['sensor_height'])
+
+            wave_params_data['time'].append(burst_signal_detrended.index[0])
+            wave_params_data['H1/3'].append(H_top_third)
+            wave_params_data['Tmean'].append(Tmean)
+
+        wave_params_data=pd.DataFrame(wave_params_data).set_index('time')
+
+        return wave_params_data
+
+    def decompose_into_IMFs_for_bursts(self,EMD_type,maximum_IMFs,number_ensembles=None,amplitude_noise_std=None,
+                                       parallel=True,nb_processes=2):
+        """
+        Decomposes the signal into Intrinsic Mode Functions (IMFs) for each burst using Empirical Mode Decomposition (EMD) 
+        or its variants.
+
+        Parameters
+        ----------
+        EMD_type : str
+            The type of EMD to use. Options are 'EMD', 'EEMD', or 'CEEMDAN'.
+        maximum_IMFs : int
+            The maximum number of IMFs to compute for each burst.
+        number_ensembles : int, optional
+            The number of ensembles to use for EEMD or CEEMDAN (required if EMD_type is 'EEMD' or 'CEEMDAN').
+        amplitude_noise_std : float, optional
+            The standard deviation of the added noise for EEMD or CEEMDAN
+        parallel : bool, optional
+            Whether to use parallel processing for CEEMDAN (default is True).
+        nb_processes : int, optional
+            The number of processes to use for parallel processing in CEEMDAN (default is 2).
+        
+        Returns
+        -------
+        numpy.ndarray
+            A 3D array containing the IMFs for each burst, with shape (number of bursts, maximum_IMFs, burst_length_s).
+        """
+        hourly_timeindex = self.measured_signal.index.floor('h').unique().sort_values()
+        time_seconds = np.arange(0,self.burst_length_s,1)
+
+        IMFs_all = np.zeros((len(hourly_timeindex),maximum_IMFs,self.burst_length_s))
+
+        if 'burstId' in self.measured_signal.columns:
+            for idx,burst in enumerate(self.measured_signal["burstId"].unique()):
+                burst_series = self.measured_signal[self.measured_signal['burstId'] == burst]
+                if EMD_type == 'EMD':
+                    emd = EMD()
+                    IMFs = emd(burst_series[self.surface_level_column].values, 
+                               time_seconds, max_imf=maximum_IMFs)[:maximum_IMFs, :]
+                elif EMD_type == 'EEMD':
+                    eemd = EEMD(trials=number_ensembles, epsilon=amplitude_noise_std)
+                    IMFs = eemd(burst_series[self.surface_level_column].values, 
+                                time_seconds, max_imf=maximum_IMFs)[:maximum_IMFs, :]
+                else:
+                    ceemd = CEEMDAN(DTYPE=np.float16,trials=number_ensembles,epsilon=amplitude_noise_std,
+                                    parallel=parallel,processes=nb_processes)
+                    IMFs = ceemd(burst_series[self.surface_level_column].values,
+                                 time_seconds,max_imf=maximum_IMFs)[:maximum_IMFs,:]
+                IMFs_all[idx,:,:] = IMFs
+        return IMFs_all
 
