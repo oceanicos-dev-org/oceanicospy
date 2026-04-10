ed895de73be1ad540267301236c7732cb2064c32
ffayalac <ffayalac@unal.edu.co>
2026-04-10 16:18:29 -0500

    removing highlighted sections into __init__.py

--- oceanicospy/models/swanpy/__init__.py
+++ oceanicospy/models/swanpy/__init__.py
@@ -2,7 +2,3 @@ from . import preprocess
 from . import execution
 from . import postprocess
 from .initializer import *
-<<<<<<< HEAD
-=======
-from . import utils
->>>>>>> 5d48c5cb29036c1269753c1321a4ce9d6bc43c90
