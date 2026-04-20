9083b5bfa91f1c8746e4294cf5fa123c7b0ff9ad
Daniela Rosero <lroserom@unal.edu.co>
2026-04-20 18:35:13 -0500

    wip(gis): add gis_examples notebook structure for io and crs sections

--- oceanicospy/gis/__init__.py
+++ oceanicospy/gis/__init__.py
@@ -1,5 +1,5 @@
 from .io_xyz import (XYZFormatSpec,  infer_xyz_format, read_xyz, write_xyz, load_xyz_as_geodataframe, save_xyz_from_geodataframe)
-from .crs_tools import (ShapefileReprojector, reproject_xyz_file)
+from .crs_tools import (PointFileReprojector, reproject_xyz_file)
 from .xyz_tile import XYZTile
 from .xyz_merger import XYZMerger
 from .xyz_mask import (AxisAlignedRectangle, XYZRectangleMask)
@@ -16,7 +16,7 @@ __all__ = [
     "save_xyz_from_geodataframe",
 
     # CRS
-    "ShapefileReprojector",
+    "PointFileReprojector",
     "reproject_xyz_file",
     
     # Tiles
